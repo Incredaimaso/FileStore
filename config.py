d21c1d7fdce0a32f5c735505ef96ffbc7fdbbad4
Incredaimaso <159280005+Incredaimaso@users.noreply.github.com>
2025-10-14 14:09:16 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from logging.handlers import RotatingFileHandler
 # Bot Configuration
 LOG_FILE_NAME = "bot.log"
 PORT = '5010'
-OWNER_ID = 6497757690
+OWNER_ID = 7950514048
 
 MSG_EFFECT = 5046509860389126442
 
@@ -13,30 +13,30 @@ SHORT_API = ""
 SHORT_TUT = "https://t.me/How_to_Download_7x/26"
 
 # Bot Configuration
-SESSION = "yato"
-TOKEN = "642712"
-API_ID = ""
-API_HASH = ""
+SESSION = "Duck"
+TOKEN = "7696766707:AAF7Q5u7i4-5GE55VQZPaZlHPXuohh4i1uA"
+API_ID = "22403100"
+API_HASH = "ccbc3f662735abfa604ef6309ba76e67"
 WORKERS = 5
 
-DB_URI = "mongodb"
-DB_NAME = "yato"
+DB_URI = "mongodb+srv://Divyanshu:gameno01@cluster0.bcj1u2h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DB_NAME = "FileStore"
 
-FSUBS = [[-1003016571084, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
+FSUBS = [[-1002897305248, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
-DB_CHANNEL =    # just put channel id dont add ""
+DB_CHANNEL = -1002820584357   # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
 # DB_CHANNELS = {
 #     "-1002595092736": {"name": "Primary DB", "is_primary": True, "is_active": True},
 #     "-1001234567890": {"name": "Secondary DB", "is_primary": False, "is_active": True}
 # }
 # Auto Delete Timer (seconds)
-AUTO_DEL = 300
+AUTO_DEL = 3600
 # Admin IDs
-ADMINS = [6497757690, 6103092779]
+ADMINS = [7950514048, 8108281129]
 # Bot Settings
 DISABLE_BTN = True
-PROTECT = True
+PROTECT = False
 
 # Messages Configuration
 MESSAGES = {
